f480ace7f53a03f28bfbf2e38d174ef895433001
Sourav Dey <souravdey@Tests-iMac.local>
2019-03-27 18:44:27 +0530

    Initial Commit2

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import ClassNames from './header.module.css';
-import SearchBar from 'material-ui-search-bar';
 class Header extends Component {
     render() {
         return (
             <header className={ClassNames.header}>
 
-                <SearchBar
-                    onChange={() => console.log('onChange')}
-                    onRequestSearch={() => console.log('onRequestSearch')}
-                    style={{
-                        margin: '0 auto',
-                        maxWidth: 800
-                    }}
-                />
+               
             </header>
         );
     }
