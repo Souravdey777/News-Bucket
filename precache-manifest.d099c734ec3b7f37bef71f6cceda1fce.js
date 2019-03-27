b8ccfd3d191b825f7eaa6fc38465f94643af729f
Sourav Dey <souravdey@Tests-iMac.local>
2019-03-27 17:31:21 +0530

    Updates

---
+++ precache-manifest.d099c734ec3b7f37bef71f6cceda1fce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4085cb4e30666a436194",
+    "url": "/News-Bucket/static/js/runtime~main.ad0a51bb.js"
+  },
+  {
+    "revision": "28fd3c85dcb3655dc678",
+    "url": "/News-Bucket/static/js/main.c0348c0e.chunk.js"
+  },
+  {
+    "revision": "ba4d2e6c7102d16ee175",
+    "url": "/News-Bucket/static/js/2.ffb22512.chunk.js"
+  },
+  {
+    "revision": "28fd3c85dcb3655dc678",
+    "url": "/News-Bucket/static/css/main.655bdf95.chunk.css"
+  },
+  {
+    "revision": "0f5a3824b36e9244da92762c79a8d0f7",
+    "url": "/News-Bucket/index.html"
+  }
+];
