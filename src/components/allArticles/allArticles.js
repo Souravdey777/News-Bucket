02ab5565ca2d8dc34df3c19191fa4fcee2e1819d
Souravdey777 <piyush.kolkata@gmail.com>
2019-04-01 12:50:51 +0530

    searching fuction done.

--- src/components/allArticles/allArticles.js
+++ src/components/allArticles/allArticles.js
@@ -8,15 +8,16 @@ import NoNetwork from '../../assets/images/NoNetwork.png';
 import Header from '../header/header';
 class AllArticles extends React.Component {
   state = {
-    key:`1fe94e1f7eb747b3a5879a48ba736409`,
+    key: `1fe94e1f7eb747b3a5879a48ba736409`,
     articles: [],
-    error:false,
-    query:null,
-    url:'https://newsapi.org/v2/top-headlines?country=in&apiKey=1fe94e1f7eb747b3a5879a48ba736409'
+    error: false,
+    query: '',
+    url: 'https://newsapi.org/v2/top-headlines?country=in&apiKey=1fe94e1f7eb747b3a5879a48ba736409'
   }
-  
-  getNews=(url)=>{axios
-    .get(url)
+
+  getNews = (url) => {
+    axios
+      .get(url)
     .then(response => {
       const articles = response.data.articles;
       console.log(articles);
@@ -27,47 +28,50 @@ class AllArticles extends React.Component {
       this.setState({ error: error });
     });
   }
-  
+
   componentWillMount() {
     this.getNews(this.state.url);
   }
 
-  handleChange=(event)=> {
+  handleChange = (event) => {
     console.log(event.target.value);
-    this.setState({query: event.target.value});
-    console.log(this.state.query);
-    this.setState({url:`https://newsapi.org/v2/everything?q=${this.state.query}&language=en&apiKey=1fe94e1f7eb747b3a5879a48ba736409`})
+    this.setState({ query: event.target.value }, () => {
+      console.log(this.state.query);
+      this.setState({ url: `https://newsapi.org/v2/everything?q=${this.state.query}&language=en&apiKey=1fe94e1f7eb747b3a5879a48ba736409` }, () => {
+        console.log(this.state.url);
+        this.getNews(this.state.url);
+      });
+    });
+    if(event.target.value===''){
+      this.setState({ url: 'https://newsapi.org/v2/top-headlines?country=in&apiKey=1fe94e1f7eb747b3a5879a48ba736409' }, () => {
+        console.log(this.state.url);
+        this.getNews(this.state.url);
+      });
+    }
 
-    this.getNews(this.state.url);
-    console.log(this.state.url);
   }
 
 
-
-    Query(){
-      
-    }
-
   render() {
     const articleState = this.state.articles;
-    let result = this.state.error?<p className={ClassNames.loadingpage}><img className={ClassNames.NoNetworkimage} src={NoNetwork} alt = 'Summerize'/>News can't be loaded</p>:<div className={ClassNames.loadingpage}>Loading...<br/><ReactLoading type="bubbles" color="#777" /></div>;
-    if (articleState && articleState.length > 1){
-      result =<StackGrid
-      columnWidth={300}
-      monitorImagesLoaded={true}>
-      {this.state.articles.map((arg, i) => {
-        return <SingleArticle key={i} articles={arg} />;
-      })}
-    </StackGrid>
+    let result = this.state.error ? <p className={ClassNames.loadingpage}><img className={ClassNames.NoNetworkimage} src={NoNetwork} alt='Summerize' />News can't be loaded</p> : <div className={ClassNames.loadingpage}>Loading...<br /><ReactLoading type="bubbles" color="#777" /></div>;
+    if (articleState && articleState.length > 1) {
+      result = <StackGrid
+        columnWidth={300}
+        monitorImagesLoaded={true}>
+        {this.state.articles.map((arg, i) => {
+          return <SingleArticle key={i} articles={arg} />;
+        })}
+      </StackGrid>
     }
     return (
       <div>
         <Header clicked={this.Query} value={this.state.query} handleChange={this.handleChange} />
-      <div className={ClassNames.body}>
-      {result}
+        <div className={ClassNames.body}>
+          {result}
+        </div>
       </div>
-      </div>
-      
+
 
     );
   }
--- src/components/header/searchingInput/searchingInput.js
+++ src/components/header/searchingInput/searchingInput.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Classnames from './searchingInput.module.css';
+import searchIcon from '../../../assets/images/searchicon.png';
 
 const SearchingInput = (props) =>
 
     <div className={Classnames.SearchingInput}>
         <input value={props.value} onChange={props.handleChange} className={Classnames.textbox} maxLength="2048" type="text" autoCapitalize="off" autoComplete="off" autoCorrect="off" spellCheck="false" title="Search" placeholder="Search" />
+        <img onClick={props.clicked} src={searchIcon} className={Classnames.searchlogo} alt='' />
     </div>
 export default SearchingInput;
