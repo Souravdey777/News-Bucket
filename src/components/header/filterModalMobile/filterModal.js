ddd1353d43a870830355fb6643da459de4807ec7
Souravdey777 <piyush.kolkata@gmail.com>
2019-04-08 13:43:18 +0530

    Mobile View Try

---
+++ src/components/header/filterModalMobile/filterModal.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import Classnames from './filterModal.module.css';
+
+const FilterModal = props => {
+
+    const Category = [
+        {
+            value: 'business',
+            name: 'Business'
+        },
+        {
+            value: 'entertainment',
+            name: 'Entertainment'
+        },
+        {
+            value: 'general',
+            name: 'General'
+        },
+        {
+            value: 'health',
+            name: 'Health'
+        },
+        {
+            value: 'science',
+            name: 'Science'
+        },
+        {
+            value: 'sports',
+            name: 'Sports'
+        },
+        {
+            value: 'technology',
+            name: 'Technology'
+        }
+    ]
+    const Country = [
+        {
+            value: 'in',
+            name: 'India'
+        },
+        {
+            value: 'us',
+            name: 'Usa'
+        },
+        {
+            value: 'au',
+            name: 'Australia'
+        },
+        {
+            value: 'ca',
+            name: 'Canada'
+        },
+        {
+            value: 'ch',
+            name: 'China'
+        }
+    ]
+    return (
+        <div lassName={Classnames.filters}>
+            <div className={Classnames.custom_dropdown}>
+                <select name="Category" id="Category"
+                    value={props.selectedValueCategory}
+                    onChange={props.handleChangeCategory}>
+                    {
+                        Category.map(o =>
+                            (<option value={o.value}>{o.name}</option>
+                            ))
+                    }
+                </select>
+            </div>
+            <div className={Classnames.custom_dropdown}>
+                <select name="Country" id="Country"
+                    value={props.selectedValueCountry}
+                    onChange={props.handleChangeCountry}>
+                    {
+                        Country.map(o =>
+                            (<option value={o.value}>{o.name}</option>
+                            ))
+                    }
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default FilterModal;
